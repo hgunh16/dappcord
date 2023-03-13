38b2a282ea486de5602869316cd7e331ccca1c3f
hgunh16 <hgunh16@gmail.com>
2023-03-13 15:27:15 +0900

    test

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -5,5 +5,31 @@ const tokens = (n) => {
 }
 
 describe("Dappcord", function () {
+  let dappcord ;
 
+  const NAME = "Dappcord"
+  const SYMBOL = "DC"
+
+  beforeEach(async () =>{
+    //Deploy Contract
+    const Dappcord = await ethers.getContractFactory("Dappcord")
+    dappcord = await Dappcord.deploy(NAME, SYMBOL)
+  })
+
+    describe("development", function(){
+      it("Sets the name", async()=> {
+        //Fetch Name
+        let result = await dappcord.name()
+        //check name
+        expect(result).to.equal(NAME)
+
+      })
+
+      it("Sets the symbol" , async()=> {
+        //fetch symbol
+        let result = await dappcord.symbol()
+        //ceehck symbol
+        expect(result).to.equal(SYMBOL)
+      })
+    })
 })
